d411a13ba0fb6dcdf17d676bfe85700ee4fad243
Pratham Sharma <pratham016914934@gmail.com>
2024-06-16 16:41:49 +0545

    completed backend part

--- urlShortner/myurl/urls.py
+++ urlShortner/myurl/urls.py
@@ -3,5 +3,7 @@ from django.urls import path,include
 from .views import *
 urlpatterns = [
    path('urlShortener',urlshort,name='urlshort'),
-   path('createUrl',createUrl,name="createUrl")
+   path('createUrl',createUrl,name="createUrl"),
+   path('<str:uid>',goto,name="goto"),
+   path('getlink/',fetchlink,name='getlink'),
 ]
--- urlShortner/myurl/views.py
+++ urlShortner/myurl/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 import uuid
 from .models import Url
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 def urlshort(request):
     data={}
     
+
     return render(request,'index.html',data)
 
 def createUrl(request):
@@ -15,3 +16,15 @@ def createUrl(request):
       myurl = Url.objects.create(my_Url=url,uid=uid)
       myurl.save()
       return HttpResponse(uid)
+    
+def goto(request,uid):
+   myurl = Url.objects.get(uid=uid)
+   return redirect(myurl.my_Url)
+
+def fetchlink(request):
+   data={}
+   myurl = Url.objects.all()
+   data = {
+      'myurl':list(myurl.values())
+   }
+   return JsonResponse(data)
